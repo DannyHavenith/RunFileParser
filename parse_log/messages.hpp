735e60c8edc64bbf761fd27c074d119c1e5de8f4
Danny <dh.git@havenith.net>
2011-09-17 04:13:39 +0200

    first version that reads a file and verifies packet checksums

---
+++ parse_log/messages.hpp
@@ -0,0 +1,163 @@
+/*
+ * messages.hpp
+ *
+ *  Created on: Sep 12, 2011
+ *      Author: danny
+ */
+
+#ifndef MESSAGES_HPP_
+#define MESSAGES_HPP_
+
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/joint_view.hpp>
+
+template< int code, int size>
+struct message {};
+
+template< int code_begin, int code_end, int size>
+struct message_range {};
+
+struct run_information  : message<1, 9>     { static const char *description() { return  "Run Information";}};
+struct run_start_stop   : message<2, 11>    { static const char *description() { return  "Run start/stop info";}};
+struct raw_gps          : message<3, -1>    { static const char *description() { return  "Raw GPS Data Input";}};
+struct new_sector_time  : message<4, 7>     { static const char *description() { return  "New Sector Time";}};
+struct new_lap_marker   : message<5, 21>    { static const char *description() { return  "New Lap Marker";}};
+struct logger_storage   : message<6, 6>     { static const char *description() { return  "Logger Storage Channel";}};
+struct gps_time_storage : message<7, 6>     { static const char *description() { return  "GPS Time Storage Channel";}};
+struct accelerations    : message<8, 6>     { static const char *description() { return  "Accelerations";}};
+struct timestamp        : message<9, 5>     { static const char *description() { return  "Time Stamp";}};
+struct gps_position     : message<10, 14>   { static const char *description() { return  "GPS Positional Data";}};
+struct gps_raw_speed    : message<11, 10>   { static const char *description() { return  "GPS Raw Speed Data";}};
+struct beacon_pulse_present : message<12, 3>{ static const char *description() { return  "Beacon Pulse Present";}};
+struct frequency1       : message<14, 5>    { static const char *description() { return  "Frequency 1";}};
+struct frequency5       : message<18, 5>    { static const char *description() { return  "Frequency 5";}};
+struct serial_data_input: message<19, -1>   { static const char *description() { return  "Serial Data Input";}};
+struct analogue1        : message_range<20,52, 4>    { static const char *description() { return  "Analogue 1";}};
+struct channel_data     : message<52, 67>   { static const char *description() { return  "Channel Data Channel";}};
+struct display_data     : message<53, 11>   { static const char *description() { return  "Display Data Channel";}};
+struct reflash          : message<54, 6>    { static const char *description() { return  "Reflash Channel";}};
+struct date_storage     : message<55, 10>   { static const char *description() { return  "Date Storage Channel";}};
+struct gps_course       : message<56, 10>   { static const char *description() { return  "GPS Course Data";}};
+struct gps_accuracy     : message<57, 10>   { static const char *description() { return  "GPS Altitude and Speed Accuracy";}};
+struct extended_frequency1 : message_range<58, 62, 11>{ static const char *description() { return  "Extended Frequency 1";}};
+struct extended_rpm     : message<62, 11>   { static const char *description() { return  "Extended RPM";}};
+struct start_of_run     : message<63, 3>    { static const char *description() { return  "Start of Run Channel";}};
+struct processed_speed  : message<64, 5>    { static const char *description() { return  "Processed Speed Data";}};
+struct gear_setup       : message<65, 30>   { static const char *description() { return  "Gear Set Up Data";}};
+struct bargraph_setup   : message<66, 11>   { static const char *description() { return  "Bargraph Set Up Data";}};
+struct dashboard_setup  : message<67, 4>    { static const char *description() { return  "Dashboard Set Up Data";}};
+struct dashboard_setup2 : message<68, 4>    { static const char *description() { return  "Dashboard Set Up Data Two";}};
+struct new_target_sector_time : message<69, 42> { static const char *description() { return  "New Target Sector Time";}};
+struct new_target_marker_time : message<70, 42> { static const char *description() { return  "New Target Marker Time";}};
+struct auxiliary_input  : message<71, 3>    { static const char *description() { return  "Auxiliary Input Module Number";}};
+struct external_temperature : message<72, 5>{ static const char *description() { return  "External Temperature Channel";}};
+struct external_frequency: message<73, 5>   { static const char *description() { return  "External Frequency Channel";}};
+struct external_percentage : message<74, 5> { static const char *description() { return  "External Percentage Channel";}};
+struct external_time    : message<75, 6>    { static const char *description() { return  "External Time Channel";}};
+struct new_lcd_data     : message<76, 24>   { static const char *description() { return  "New LCD Data Channel";}};
+struct new_led_data     : message<77, 3>    { static const char *description() { return  "New LED Data Channel";}};
+struct precalc_distance_data : message<78, 6>{ static const char *description() { return  "Pre Calculated Distance Data Channel";}};
+struct yaw_rates        : message<79, 4>    { static const char *description() { return  "Yaw Rates Channel";}};
+struct calculated_yaw   : message<80, 4>    { static const char *description() { return  "Calculated Yaw Channel";}};
+struct pitch_rate       : message<81, 5>    { static const char *description() { return  "Pitch Rate Channel";}};
+struct pitch_angle      : message<82, 5>    { static const char *description() { return  "Pitch Angle Channel";}};
+struct roll_rate        : message<83, 5>    { static const char *description() { return  "Roll Rate Channel";}};
+struct roll_angle       : message<84, 5>    { static const char *description() { return  "Roll Angle Channel";}};
+struct gradient         : message<85, 10>   { static const char *description() { return  "Gradient Channel";}};
+struct pulse_count1     : message_range<86, 90, 5>    { static const char *description() { return  "Pulse Count 1";}};
+struct baseline         : message<90, 6>    { static const char *description() { return  "Baseline Channel";}};
+struct unit_control     : message<91, 5>    { static const char *description() { return  "Unit Control Channel";}};
+struct z_acceleration   : message<92, 4>    { static const char *description() { return  "Z Acceleration";}};
+struct external_angle   : message<93, 5>    { static const char *description() { return  "External Angle Channel";}};
+struct external_pressure: message<94, 6>    { static const char *description() { return  "External Pressure Channel";}};
+struct external_misc    : message<95, 5>    { static const char *description() { return  "External Miscellaneous Channel";}};
+struct time_into_current_lap : message<96, 10>  { static const char *description() { return  "Time in to current lap and sector";}};
+struct high_res_timer   : message<97, 8>    { static const char *description() { return  "High resolution event timer";}};
+struct sector_definition: message<101, 19>  { static const char *description() { return  "Sector Definition Channel";}};
+struct brakebox_to_pc   : message<102, -1>  { static const char *description() { return  "BRAKEBOX to PC Communication Channel";}};
+struct dvr_communication: message<103, 17>  { static const char *description() { return  "DVR Communication Channel";}};
+struct video_frame_index: message<104, 9>   { static const char *description() { return  "Video frame index";}};
+struct local_ned_velocities : message<105, 11>  { static const char *description() { return  "Local NED velocities";}};
+struct general_configuration : message<107, -1> { static const char *description() { return  "General Configuration Message";}};
+
+typedef boost::mpl::vector<
+    run_information ,
+    run_start_stop  ,
+    raw_gps         ,
+    new_sector_time ,
+    new_lap_marker  ,
+    logger_storage  ,
+    gps_time_storage    ,
+    accelerations   ,
+    timestamp       ,
+    gps_position        ,
+    gps_raw_speed   ,
+    beacon_pulse_present ,
+    frequency1      ,
+    frequency5      ,
+    serial_data_input,
+    analogue1       ,
+    channel_data        ,
+    display_data        ,
+    reflash
+    > commands1;
+
+typedef boost::mpl::vector<
+    date_storage        ,
+    gps_course      ,
+    gps_accuracy        ,
+    extended_frequency1 ,
+    extended_rpm        ,
+    start_of_run        ,
+    processed_speed ,
+    gear_setup      ,
+    bargraph_setup,
+    dashboard_setup ,
+    dashboard_setup2 ,
+    new_target_sector_time ,
+    new_target_marker_time ,
+    auxiliary_input ,
+    external_temperature ,
+    external_frequency,
+    external_percentage ,
+    external_time   ,
+    new_lcd_data
+    > commands2;
+
+typedef boost::mpl::vector<
+    new_led_data        ,
+    precalc_distance_data ,
+    yaw_rates       ,
+    calculated_yaw  ,
+    pitch_rate      ,
+    pitch_angle     ,
+    roll_rate       ,
+    roll_angle      ,
+    gradient            ,
+    pulse_count1        ,
+    baseline            ,
+    unit_control        ,
+    z_acceleration  ,
+    external_angle  ,
+    external_pressure,
+    external_misc   ,
+    time_into_current_lap ,
+    high_res_timer  ,
+    sector_definition
+    > commands3;
+
+typedef boost::mpl::vector<
+    brakebox_to_pc  ,
+    dvr_communication,
+    video_frame_index,
+    local_ned_velocities ,
+    general_configuration
+    > commands4;
+
+using boost::mpl::joint_view;
+
+typedef joint_view< joint_view<commands1, commands2>, joint_view<commands3, commands4> > commands;
+
+
+
+#endif /* MESSAGES_HPP_ */
--- parse_log/parse_log.cpp
+++ parse_log/parse_log.cpp
@@ -1,150 +1,205 @@
 // parse_log.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-#define BOOST_MPL_LIMIT_VECTOR_SIZE 50
-#include <boost/mpl/vector.hpp>
-template< int code, int size>
-struct message {};
+#include "messages.hpp"
 
-struct run_information	: message<1, 9>		{ static const char *description() { return  "Run Information";}};
-struct run_start_stop	: message<2, 11>	{ static const char *description() { return  "Run start/stop info";}};
-struct raw_gps			: message<3, -1>	{ static const char *description() { return  "Raw GPS Data Input";}};
-struct new_sector_time	: message<4, 7>		{ static const char *description() { return  "New Sector Time";}};
-struct new_lap_marker	: message<5, 21>	{ static const char *description() { return  "New Lap Marker";}};
-struct logger_storage	: message<6, 6>		{ static const char *description() { return  "Logger Storage Channel";}};
-struct gps_time_storage	: message<7, 6>		{ static const char *description() { return  "GPS Time Storage Channel";}};
-struct accelerations	: message<8, 6>		{ static const char *description() { return  "Accelerations";}};
-struct timestamp		: message<9, 5>		{ static const char *description() { return  "Time Stamp";}};
-struct gps_position		: message<10, 14>	{ static const char *description() { return  "GPS Positional Data";}};
-struct gps_raw_speed	: message<11, 10>	{ static const char *description() { return  "GPS Raw Speed Data";}};
-struct beacon_pulse_present : message<12, 3>{ static const char *description() { return  "Beacon Pulse Present";}};
-struct frequency1		: message<14, 5>	{ static const char *description() { return  "Frequency 1";}};
-struct frequency5		: message<18, 5>	{ static const char *description() { return  "Frequency 5";}};
-struct serial_data_input: message<19, -1>	{ static const char *description() { return  "Serial Data Input";}};
-struct analogue1		: message<20, 4>	{ static const char *description() { return  "Analogue 1";}};
-struct analogue32		: message<51, 4>	{ static const char *description() { return  "Analogue 32";}};
-struct channel_data		: message<52, 67>	{ static const char *description() { return  "Channel Data Channel";}};
-struct display_data		: message<53, 11>	{ static const char *description() { return  "Display Data Channel";}};
-struct reflash			: message<54, 6>	{ static const char *description() { return  "Reflash Channel";}};
-struct date_storage		: message<55, 10>	{ static const char *description() { return  "Date Storage Channel";}};
-struct gps_course		: message<56, 10>	{ static const char *description() { return  "GPS Course Data";}};
-struct gps_accuracy		: message<57, 10>	{ static const char *description() { return  "GPS Altitude and Speed Accuracy";}};
-struct extended_frequency1 : message<58, 11>{ static const char *description() { return  "Extended Frequency 1";}};
-struct extended_frequency4 : message<61, 11>{ static const char *description() { return  "Extended Frequency 4";}};
-struct extended_rpm		: message<62, 11>	{ static const char *description() { return  "Extended RPM";}};
-struct start_of_run		: message<63, 3>	{ static const char *description() { return  "Start of Run Channel";}};
-struct processed_speed	: message<64, 5>	{ static const char *description() { return  "Processed Speed Data";}};
-struct gear_setup		: message<65, 30>	{ static const char *description() { return  "Gear Set Up Data";}};
-struct bargraph_setup	: message<66, 11>	{ static const char *description() { return  "Bargraph Set Up Data";}};
-struct dashboard_setup	: message<67, 4>	{ static const char *description() { return  "Dashboard Set Up Data";}};
-struct dashboard_setup2 : message<68, 4>	{ static const char *description() { return  "Dashboard Set Up Data Two";}};
-struct new_target_sector_time : message<69, 42>	{ static const char *description() { return  "New Target Sector Time";}};
-struct new_target_marker_time : message<70, 42>	{ static const char *description() { return  "New Target Marker Time";}};
-struct auxiliary_input	: message<71, 3>	{ static const char *description() { return  "Auxiliary Input Module Number";}};
-struct external_temperature : message<72, 5>{ static const char *description() { return  "External Temperature Channel";}};
-struct external_frequency: message<73, 5>	{ static const char *description() { return  "External Frequency Channel";}};
-struct external_percentage : message<74, 5>	{ static const char *description() { return  "External Percentage Channel";}};
-struct external_time	: message<75, 6>	{ static const char *description() { return  "External Time Channel";}};
-struct new_lcd_data		: message<76, 24>	{ static const char *description() { return  "New LCD Data Channel";}};
-struct new_led_data		: message<77, 3>	{ static const char *description() { return  "New LED Data Channel";}};
-struct precalc_distance_data : message<78, 6>{ static const char *description() { return  "Pre Calculated Distance Data Channel";}};
-struct yaw_rates		: message<79, 4>	{ static const char *description() { return  "Yaw Rates Channel";}};
-struct calculated_yaw	: message<80, 4>	{ static const char *description() { return  "Calculated Yaw Channel";}};
-struct pitch_rate		: message<81, 5>	{ static const char *description() { return  "Pitch Rate Channel";}};
-struct pitch_angle		: message<82, 5>	{ static const char *description() { return  "Pitch Angle Channel";}};
-struct roll_rate		: message<83, 5>	{ static const char *description() { return  "Roll Rate Channel";}};
-struct roll_angle		: message<84, 5>	{ static const char *description() { return  "Roll Angle Channel";}};
-struct gradient			: message<85, 10>	{ static const char *description() { return  "Gradient Channel";}};
-struct pulse_count1		: message<86, 5>	{ static const char *description() { return  "Pulse Count 1";}};
-struct pulse_count4		: message<89, 5>	{ static const char *description() { return  "Pulse Count 4";}};
-struct baseline			: message<90, 6>	{ static const char *description() { return  "Baseline Channel";}};
-struct unit_control		: message<91, 5>	{ static const char *description() { return  "Unit Control Channel";}};
-struct z_acceleration	: message<92, 4>	{ static const char *description() { return  "Z Acceleration";}};
-struct external_angle	: message<93, 5>	{ static const char *description() { return  "External Angle Channel";}};
-struct external_pressure: message<94, 6>	{ static const char *description() { return  "External Pressure Channel";}};
-struct external_misc	: message<95, 5>	{ static const char *description() { return  "External Miscellaneous Channel";}};
-struct time_into_current_lap : message<96, 10>	{ static const char *description() { return  "Time in to current lap and sector";}};
-struct high_res_timer	: message<97, 8>	{ static const char *description() { return  "High resolution event timer";}};
-struct sector_definition: message<101, 19>	{ static const char *description() { return  "Sector Definition Channel";}};
-struct brakebox_to_pc	: message<102, -1>	{ static const char *description() { return  "BRAKEBOX to PC Communication Channel";}};
-struct dvr_communication: message<103, 17>	{ static const char *description() { return  "DVR Communication Channel";}};
-struct video_frame_index: message<104, 9>	{ static const char *description() { return  "Video frame index";}};
-struct local_ned_velocities : message<105, 11>	{ static const char *description() { return  "Local NED velocities";}};
-struct general_configuration : message<107, -1>	{ static const char *description() { return  "General Configuration Message";}};
+#include <boost/fusion/algorithm/iteration/for_each.hpp>
+#include <boost/fusion/include/for_each.hpp>
+#include <boost/fusion/adapted/mpl.hpp>
+#include <boost/fusion/include/mpl.hpp>
 
-typedef boost::mpl::vector<
-	run_information	,
-	run_start_stop	,
-	raw_gps			,
-	new_sector_time	,
-	new_lap_marker	,
-	logger_storage	,
-	gps_time_storage	,
-	accelerations	,
-	timestamp		,
-	gps_position		,
-	gps_raw_speed	,
-	beacon_pulse_present ,
-	frequency1		,
-	frequency5		,
-	serial_data_input,
-	analogue1		,
-	analogue32		,
-	channel_data		,
-	display_data		,
-	reflash			,
-	date_storage		,
-	gps_course		,
-	gps_accuracy		,
-	extended_frequency1 ,
-	extended_frequency4 ,
-	extended_rpm		,
-	start_of_run		,
-	processed_speed	,
-	gear_setup		,
-	bargraph_setup
-> commands1;
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <exception>
+#include <stdexcept>
+#include <vector>
 
-typedef boost::mpl::vector<
-	dashboard_setup	,
-	dashboard_setup2 ,
-	new_target_sector_time ,
-	new_target_marker_time ,
-	auxiliary_input	,
-	external_temperature ,
-	external_frequency,
-	external_percentage ,
-	external_time	,
-	new_lcd_data		,
-	new_led_data		,
-	precalc_distance_data ,
-	yaw_rates		,
-	calculated_yaw	,
-	pitch_rate		,
-	pitch_angle		,
-	roll_rate		,
-	roll_angle		,
-	gradient			,
-	pulse_count1		,
-	pulse_count4		,
-	baseline			,
-	unit_control		,
-	z_acceleration	,
-	external_angle	,
-	external_pressure,
-	external_misc	,
-	time_into_current_lap ,
-	high_res_timer	,
-	sector_definition,
-	brakebox_to_pc	,
-	dvr_communication,
-	video_frame_index,
-	local_ned_velocities ,
-	general_configuration> commands2;
+typedef std::vector<unsigned char> buffer_type;
+
+
+template<typename iterator, int size>
+struct size_getter
+{
+    static int get_size( iterator, iterator)
+    {
+        return size;
+    }
+};
+
+template< typename iterator>
+struct size_getter< iterator, -1>
+{
+    static int get_size( iterator first, iterator end)
+    {
+        iterator second = ++first;
+        if (second != end)
+        {
+            return *second;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+};
+
+template<typename iterator>
+struct checker_interface
+{
+    virtual bool check( iterator &i, iterator end) = 0;
+};
+
+template<typename iterator, int size>
+struct checker : public checker_interface<iterator>
+{
+    static checker<iterator, size> &instance()
+    {
+        static checker<iterator, size> the_instance;
+        return the_instance;
+    }
+
+    int get_size( iterator begin, iterator end)
+    {
+        return size_getter<iterator, size>::get_size( begin, end);
+    }
+
+    virtual bool check( iterator &i, iterator end)
+    {
+        iterator save = i;
+        unsigned char sum = 0;
+        for (int count = get_size(i, end) - 1; count && i != end; --count)
+        {
+            unsigned char value = *i++;
+            sum += value;
+        }
+
+        if (i == end || sum != *i++)
+        {
+            i = save;
+            return false;
+        }
+
+        return true;
+    }
+
+};
+
+typedef checker_interface<buffer_type::const_iterator> *table_type[256];
+struct cell_filler
+{
+    cell_filler( table_type &table)
+    :table(table)
+    {
+
+    }
+
+    template< int header, int size>
+    void operator()( const message<header, size> & ) const
+    {
+        std::cout << header << ":";
+        table[header] = &checker< buffer_type::const_iterator, size>::instance();
+    }
+
+    template< int header_begin, int header_end, int size>
+    void operator()( const message_range< header_begin, header_end, size>& ) const
+    {
+        for ( int i = header_begin; i != header_end; ++i)
+        {
+            table[i] = &checker< buffer_type::const_iterator, size>::instance();
+        }
+    }
+
+private:
+    table_type &table;
+};
+
+void fill_checker_table( table_type &table)
+{
+    std::fill( table, table + 256, static_cast<checker_interface<buffer_type::const_iterator> *>( 0));
+    boost::fusion::for_each( commands(), cell_filler(table));
+}
+
+void error( const std::string &what)
+{
+    throw std::runtime_error( what);
+}
+
+// global, so zero-initialized
+size_t good_count[256];
+size_t bad_count[256];
+
+void scan_events( buffer_type::const_iterator begin, buffer_type::const_iterator end)
+{
+    using namespace std;
+
+    table_type table;
+    fill_checker_table( table);
+
+    unsigned char firstbyte = 0;
+    while (begin != end)
+    {
+
+        firstbyte = *begin;
+        while (begin != end && table[*begin] && table[*begin]->check( begin, end))
+        {
+            ++good_count[firstbyte];
+            firstbyte = *begin;
+            cout << '.';
+        }
+
+        if (begin != end)
+        {
+            size_t skipcount = 0;
+            firstbyte = *begin;
+            while ( begin != end &&  (!table[*begin] ||  !table[*begin]->check( begin, end)))
+            {
+                ++begin;
+                ++skipcount;
+            }
+            ++bad_count[firstbyte];
+            cout << "\nskipped " << skipcount << " bytes, starting with " << (int)firstbyte << "\n";
+        }
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    using namespace std;
+
+    try
+    {
+        if (argc < 2)
+        {
+            error("specify a file name");
+        }
+
+        ifstream file( argv[1], ios_base::binary);
+        if (!file)
+        {
+            error( string("could not open file: ") + argv[1]);
+        }
+
+        file.unsetf( ios_base::skipws);
+        istreambuf_iterator<char> begin( file), end;
+        const vector<unsigned char> buffer( begin, end);
+
+        cout << "buffer is " << buffer.size() << " bytes large\n";
+
+        scan_events( buffer.begin(), buffer.end());
+
+        for (int i = 0; i < 256; ++i)
+        {
+            cout << i << "    " << good_count[i] << "    " << bad_count[i] << '\n';
+        }
+
+        cout << "finished\n";
+    }
+    catch (exception &e)
+    {
+        cerr << "something went wrong: " << e.what() << '\n';
+    }
+
 	return 0;
 }
 
